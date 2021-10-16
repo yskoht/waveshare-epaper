5bcf1c02d2eec32d7d0f51bd2804a327818e674b
yskoht <ysk.oht@gmail.com>
2021-10-16 20:37:06 +0900

    Add epaper

--- epaper/__init__.py
+++ epaper/__init__.py
@@ -1,1 +1,9 @@
+import importlib
+
 __version__ = '0.1.0'
+
+lib = 'epaper.e-Paper.RaspberryPi_JetsonNano.python.lib.waveshare_epd'
+
+def epaper(model):
+  return importlib.import_module('{}.{}'.format(lib, model))
+
