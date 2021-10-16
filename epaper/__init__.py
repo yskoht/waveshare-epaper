b4c0cc17ad6327365a57087e2a55955577fb0fc9
yskoht <ysk.oht@gmail.com>
2021-10-16 21:41:13 +0900

    Bump version 0.3.0

--- epaper/__init__.py
+++ epaper/__init__.py
@@ -1,9 +1,8 @@
 import importlib
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 lib = 'epaper.e-Paper.RaspberryPi_JetsonNano.python.lib.waveshare_epd'
 
 def epaper(model):
   return importlib.import_module('{}.{}'.format(lib, model))
-
